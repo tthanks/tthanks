b0f2222c18756ee6713abfdd021dbb1e150d4f9d
tthanks <tthankstang@outlook.com>
2018-12-05 21:24:20 +0800

    modify name

--- httpget_mod.py
+++ httpget_mod.py
@@ -6,8 +6,10 @@ import re  # 导入正则表达式
 import datetime, time  # 导入时间
 import sys
 
-f = open("a.txt", "r")  # 获取a.txt中的股票代码
-line = f.readline()
+
+
+stock_list = open("stock_list.txt", "r")  # 获取a.txt中的股票代码
+line = stock_list.readline()
 while line != "":
     stockcode = line[:6]
     now = datetime.datetime.now()  # 定义时间
@@ -16,20 +18,26 @@ while line != "":
     # http get
     conn = http.client.HTTPConnection("qt.gtimg.cn")  # 获取http页面
     conn.request("GET", "/q=sh" + str(stockcode))
-    r1 = conn.getresponse()
-    print(r1.status, r1.reason)
+    result = conn.getresponse()
+    print(result.status, result.reason)
 
     # 输出部分
-    List1 = [r1.read().decode('GBK')]  # 定义r1读取的内容，GBK格式
-    file1 = open('text_file.txt', 'a')  # 导入http模块，a表示追加
+    Original_list = [result.read().decode('GBK')]  # 定义result读取的内容，GBK格式
+    Original_list_file = open('Original_list_file.txt', 'a')  # 导入http模块，a表示追加
 
-    file1.write('\n')
-    file1.write(str(now))
-    file1.write(str(List1))
-    file1.write('\n')
-    file1.close()
+    Original_list_file.write('\n')
+    Original_list_file.write(str(now))
+    Original_list_file.write(str(Original_list))
+    Original_list_file.write('\n')
+    Original_list_file.close()
     conn.close()
+    line = stock_list.readline()
+stock_list.close()
 
-    line = f.readline()
-f.close()
+Original_list_file = open('Original_list_file.txt', 'r')
+Original_list_line = Original_list_file.readline()
+
+print (Original_list_line)
+
+
 
