3d3d3c0d52fdce132c3087255e40cfb5ba632332
tthanks <tthankstang@outlook.com>
2020-07-20 23:28:50 +0800

    add txt_mod

--- httpget_mod.py
+++ httpget_mod.py
@@ -5,10 +5,12 @@ import http.client  # 导入http模块
 import re  # 导入正则表达式
 import datetime, time  # 导入时间
 import sys
+import re
+import datetime
 
 def main():
 
-    stock_list = open("stock_list.txt", "r")  # 获取a.txt中的股票代码
+    stock_list = open("./stock_list.txt", "r")  # 获取a.txt中的股票代码
     line = stock_list.readline()
     while line != "":
         stockcode = line[:8]
@@ -20,22 +22,54 @@ def main():
         result = conn.getresponse()
         print(result.status, result.reason)
         # 输出部分
-        Original_list = [result.read().decode('GBK')]  # 定义result读取的内容，GBK格式
-        Original_list_file = open('Original_list_file.txt', 'a')  # 导入http模块，a表示追加
-        #Original_list_file.write('\n')
-        Original_list_file.write(str(now)+str(Original_list))
-        #Original_list_file.write(str(Original_list))
-        Original_list_file.write('\n')
-        Original_list_file.close()
+        original_list = [result.read().decode('GBK')]  # 定义result读取的内容，GBK格式
+        original_list_file = open('./original_list_file.txt', 'a')  # 导入http模块，a表示追加
+        # original_list_file.write('\n')
+        original_list_file.write(str(now)+str(original_list))
+        # original_list_file.write(str(original_list))
+        original_list_file.write('\n')
+        original_list_file.close()
         conn.close()
-        line = stock_list.readline()#防止进入无限的循环
+        line = stock_list.readline()    # 防止进入无限的循环
 
     stock_list.close()
-
 
 
 if __name__ == '__main__':
     main()
 
+
+file = open("./Original_list_file.txt", "r")
+content = file.readline()
+now = datetime.datetime.now()  # 定义时间
+time = now.strftime('%Y-%m-%d')  # 定义时间格式
+
+while content != "":
+    content_initial = content
+    # print(content_initial)
+    # ontent_process_step1 = re.compile(r'~[0-9]+\.?[0-9]+')
+    content_process_step1 = re.compile(r'(?<=~).+?(?=~)')  # 匹配的字符是XX，但必须满足形式是AXXB这样的字符串
+    content_process_step2 = content_process_step1.findall(content_initial)  # 找到所有符合正则表达式的list元素
+    content_process_step2.insert(0, content[0:19])  # 获取日期
+    content_process_step3 = content_process_step2[:6]  # 截取list的前面6位，分别是股票名字、代码、当前价格、昨收、今开
+    # content_process_step3.append(content[10:])
+    print(content_process_step3)
+
+    # content_spilit = content_initial.split("~")
+    # print(content_spilit)
+    # stock_price = re.compile(r' ' + str(stockcode) + ' ')
+    # print(content)
+    # regMatch = stock_price.findall(content)
+    # print(regMatch)
+    content = file.readline()
+    # 读文本的全文并打印出来
+    # i = file.read().splitlines()
+    # for a =
+    # print (i)
+    # 这个时候再读的话，返回EOF
+# stock_price = re.compile(r'')
+
+# regMatch = stock_price.split(line)
+        # print (stock_price)
 
 
--- txt_mod.py
+++ txt_mod.py
@@ -1,41 +1,38 @@
 import re
 import datetime
 
-#with open('Original_list_file.txt', encoding='utf-8', ) as file:
+# with open('Original_list_file.txt', encoding='utf-8', ) as file:
 
-file = open("Original_list_file.txt", "r")
+file = open("./Original_list_file.txt", "r")
 content = file.readline()
 now = datetime.datetime.now()  # 定义时间
 time = now.strftime('%Y-%m-%d')  # 定义时间格式
 
 while content != "":
     content_initial = content
-    #print(content_initial)
-    #content_process_step1 = re.compile(r'~[0-9]+\.?[0-9]+')
+    # print(content_initial)
+    # ontent_process_step1 = re.compile(r'~[0-9]+\.?[0-9]+')
     content_process_step1 = re.compile(r'(?<=~).+?(?=~)')  # 匹配的字符是XX，但必须满足形式是AXXB这样的字符串
     content_process_step2 = content_process_step1.findall(content_initial)  # 找到所有符合正则表达式的list元素
-    content_process_step2.insert(0,time)  # 增加日期
-    content_process_step3 = content_process_step2[:6]  # 截取list的前面6位，分别是日期、股票名字、代码、当前价格、昨收、今开
-
-    #contest_process_step2 = str.lstrip('~');
-    #print(content_process_step1.findall(content_initial))
+    content_process_step2.insert(0, content[0:10])  # 获取日期
+    content_process_step3 = content_process_step2[:6]  # 截取list的前面6位，分别是股票名字、代码、当前价格、昨收、今开
+    # content_process_step3.append(content[10:])
     print(content_process_step3)
-    #str = content_process_step1.findall(content_initial)
-    #print(content_process_step2.findall(content_initial)
 
-    #content_spilit = content_initial.split("~")
-    #print(content_spilit)
+    # content_spilit = content_initial.split("~")
+    # print(content_spilit)
     # stock_price = re.compile(r' ' + str(stockcode) + ' ')
     # print(content)
     # regMatch = stock_price.findall(content)
     # print(regMatch)
     content = file.readline()
     # 读文本的全文并打印出来
-    #i = file.read().splitlines()
-    #for a =
-    #print (i)
+    # i = file.read().splitlines()
+    # for a =
+    # print (i)
     # 这个时候再读的话，返回EOF
-#stock_price = re.compile(r'')
+# stock_price = re.compile(r'')
 
-#regMatch = stock_price.split(line)
-        #print (stock_price)
+# regMatch = stock_price.split(line)
+        # print (stock_price)
+
